357f19b720a9432c6b7f142e395120556586313f
Matheus Armelin <55255174+armelin2001@users.noreply.github.com>
2022-10-11 19:48:16 -0300

    fazendo ajustes para lista de cards

--- imobiliaria/src/app/app.component.ts
+++ imobiliaria/src/app/app.component.ts
@@ -6,5 +6,57 @@ import { Component, ViewChild } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  imoveis: any[] = [
+    {
+      tipoImovel: 'Apartamento',
+      nomeRua: 'Rua do teste',
+      valor: 0,
+      bairroEestado: 'Bairro, Curitiba'
+    },
+    {
+      tipoImovel: 'Apartamento',
+      nomeRua: 'Rua do teste',
+      valor: 0,
+      bairroEestado: 'Bairro, Curitiba'
+    },
+    {
+      tipoImovel: 'Apartamento',
+      nomeRua: 'Rua do teste',
+      valor: 0,
+      bairroEestado: 'Bairro, Curitiba'
+    },
+    {
+      tipoImovel: 'Apartamento',
+      nomeRua: 'Rua do teste',
+      valor: 0,
+      bairroEestado: 'Bairro, Curitiba'
+    },
+    {
+      tipoImovel: 'Apartamento',
+      nomeRua: 'Rua do teste',
+      valor: 0,
+      bairroEestado: 'Bairro, Curitiba'
+    },
+    
+    {
+      tipoImovel: 'Apartamento',
+      nomeRua: 'Rua do teste',
+      valor: 0,
+      bairroEestado: 'Bairro, Curitiba'
+    },
+    
+    {
+      tipoImovel: 'Apartamento',
+      nomeRua: 'Rua do teste',
+      valor: 0,
+      bairroEestado: 'Bairro, Curitiba'
+    },
+    
+    {
+      tipoImovel: 'Apartamento',
+      nomeRua: 'Rua do teste',
+      valor: 0,
+      bairroEestado: 'Bairro, Curitiba'
+    },
+  ]
 }
--- imobiliaria/src/app/shared/componentes/card-imoveis/card-imoveis.component.ts
+++ imobiliaria/src/app/shared/componentes/card-imoveis/card-imoveis.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-imoveis',
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-imoveis.component.scss']
 })
 export class CardImoveisComponent implements OnInit {
+  
+  @Input()
+  tipoImovel: string = '';
 
+  @Input()
+  nomeRua: string = '';
+
+  @Input()
+  valor: number = 0;
+
+  @Input()
+  bairroEestado: string = '';
+
+  teste = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque minima veniam, '
   constructor() { }
 
   ngOnInit(): void {
